1a067a8d5e0a36dc482c14c40f087991670d588c
BaptisteDu79 <vadorapp123@gmail.com>
2023-07-29 12:06:20 +0200

    BzallsssasqSQS

--- Pong/Ball.py
+++ Pong/Ball.py
@@ -1,7 +1,7 @@
 from math import sin, cos, tan, pow
 from pygame import Vector2
 class Ball:
-    def __init__(self, screen, X, Y,Vy, Vx, dir):
+    def __init__(self, screen, X, Y,Vy, Vx, dir, sprite):
         self.r = 0
         self.x = X
         self.y = Y
@@ -9,4 +9,5 @@ class Ball:
         self.Vx = Vx
         self.Vy = Vy
         d = screen.get_width() pow(screen.get_height()-self.y,2)
+        self.sprite = sprite
         self.Target = Vector2(d)
