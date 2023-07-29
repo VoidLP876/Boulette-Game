7ce89220b0811c7d1123506f441f1b25f2b2da73
BobLeBuvard <Paul.Simons.1999@gmail.com>
2023-07-29 15:18:32 +0200

    ds

--- Pygame.py
+++ Pygame.py
@@ -8,10 +8,13 @@ fenetre = pygame.display.set_mode((1400,720), FULLSCREEN )
 ScreenWidth, ScreenHeight = fenetre.get_size()
 
 ball = Ball(700, 360, 600)
+
 paddles = [
     Paddle(700, 100, 100, 20, "Assets/paddlebleuv1.png"),
     Paddle(700, 100, 20, 100, "Assets/paddlebleuv1.png"),
 ]
+
+
 oldTime = pygame.time.get_ticks()
 IsGameRunning = 2
 accueil = pygame.image.load("Assets/backgroundv3.png").convert()
@@ -24,7 +27,6 @@ fenetre.blit(fond, (0,0))
 pygame.mixer.music.load('Assets/music.mp3')
 
 pygame.display.flip()
-
 
 def refresh():
     pygame.display.flip()
@@ -71,6 +73,7 @@ while IsGameRunning == 1:
     ball.MoveBall(deltaTime)
 
     keys_pressed = pygame.key.get_pressed()
+    
     if keys_pressed[pygame.K_LEFT]:
         paddles[0].move(deltaTime, -1, 0)
         if(paddles[0].isTouchingWallX()):
