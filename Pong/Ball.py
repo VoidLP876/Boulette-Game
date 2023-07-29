dd7c586d3c8f1846a2018ed1199aea551f044aed
BaptisteDu79 <vadorapp123@gmail.com>
2023-07-29 12:01:52 +0200

    Ballsllslslslslslslsls

---
+++ Pong/Ball.py
@@ -0,0 +1,12 @@
+from math import sin, cos, tan, pow
+from pygame import Vector2
+class Ball:
+    def __init__(self, screen, X, Y,Vy, Vx, dir):
+        self.r = 0
+        self.x = X
+        self.y = Y
+        self.dir = dir
+        self.Vx = Vx
+        self.Vy = Vy
+        d = screen.get_width() pow(screen.get_height()-self.y,2)
+        self.Target = Vector2(d)
