6e19689df728f1ec975b84dc47b142c818e04802
valet noir <valetnoir.gaming@gmail.com>
2023-07-29 12:06:37 +0200

    dt

--- Pygame.py
+++ Pygame.py
@@ -9,12 +9,19 @@ pygame.init()
 fenetre = pygame.display.set_mode((1366,720), FULLSCREEN )
 ScreenWidth, ScreenHeigh = fenetre.get_size()
 
+oldTime = pygame.time.get_ticks()
+
 IsGameRunning = 1
 fond = pygame.image.load("Assets/Nyan.PNG").convert()
 fond = pygame.transform.scale(fond, (1366, 768))
 fenetre.blit(fond, (0,0))
 pygame.display.flip()
 while IsGameRunning:
+    # deltatime
+    t = pygame.time.get_ticks()
+    deltaTime = (t - oldTime) / 1000.0
+    oldTime = t
+
     for event in pygame.event.get():
         if event.type == QUIT or event.key == pygame.K_ESCAPE: # ECHAP
             IsGameRunning = 0
