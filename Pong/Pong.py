4d0134185c7b29a9cf430599d0a647d5d25ba96b
BaptisteDu79 <vadorapp123@gmail.com>
2023-07-29 11:02:23 +0200

    Folder

---
+++ Pong/Pong.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+import random
+test = random.randint(0,180) 
+pygame.init()
+fenetre = pygame.display.set_mode((640,480), RESIZABLE)
+while True: 
+    print (test)
+
+# j'ai fait un commentaire lol
